c9baa2066ce262c94c911c93421bc6510a374952
hidjic <stephaneboivin.hidjic@yahoo.fr>
2018-04-13 16:44:25 +0200

    last update

---
+++ Dump20180413-1.sql
@@ -0,0 +1,144 @@
+CREATE DATABASE  IF NOT EXISTS `proxibanquebdd` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `proxibanquebdd`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: proxibanquebdd
+-- ------------------------------------------------------
+-- Server version	5.7.11
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `idClient` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `adresse` varchar(200) NOT NULL,
+  `codePostal` varchar(45) NOT NULL,
+  `ville` varchar(45) NOT NULL,
+  `idConseiller` int(11) NOT NULL,
+  PRIMARY KEY (`idClient`),
+  KEY `fk_conseiller_idx` (`idConseiller`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+INSERT INTO `client` VALUES (2,'test','test','test@test','test','test','test',1),(4,'qsd','qsd','qsd@qsd','qsd','qsd','qsd',1);
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compte`
+--
+
+DROP TABLE IF EXISTS `compte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `compte` (
+  `idCompte` int(11) NOT NULL AUTO_INCREMENT,
+  `numCompte` varchar(45) NOT NULL,
+  `dateCreation` varchar(20) NOT NULL,
+  `solde` float NOT NULL DEFAULT '0',
+  `idTypeCompte` int(11) NOT NULL,
+  `idClient` int(11) NOT NULL,
+  `decouvert` float DEFAULT NULL,
+  `taux` float DEFAULT NULL,
+  PRIMARY KEY (`idCompte`),
+  KEY `fk_type_idx` (`idTypeCompte`),
+  KEY `fk_client_idx` (`idClient`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compte`
+--
+
+LOCK TABLES `compte` WRITE;
+/*!40000 ALTER TABLE `compte` DISABLE KEYS */;
+INSERT INTO `compte` VALUES (1,'numCompte','13-04-2018',0,1,2,1000,NULL),(2,'numCompte','13-04-2018',0,2,2,NULL,0),(3,'numCompte','13-04-2018',0,2,4,NULL,0);
+/*!40000 ALTER TABLE `compte` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `conseiller`
+--
+
+DROP TABLE IF EXISTS `conseiller`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `conseiller` (
+  `idConseiller` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(45) NOT NULL,
+  `prenom` varchar(45) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `nbClient` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`idConseiller`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conseiller`
+--
+
+LOCK TABLES `conseiller` WRITE;
+/*!40000 ALTER TABLE `conseiller` DISABLE KEYS */;
+INSERT INTO `conseiller` VALUES (1,'Lepremier','Conseiller','toto','123',0);
+/*!40000 ALTER TABLE `conseiller` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `typecompte`
+--
+
+DROP TABLE IF EXISTS `typecompte`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `typecompte` (
+  `idTypeCompte` int(11) NOT NULL AUTO_INCREMENT,
+  `nomType` varchar(45) NOT NULL,
+  PRIMARY KEY (`idTypeCompte`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `typecompte`
+--
+
+LOCK TABLES `typecompte` WRITE;
+/*!40000 ALTER TABLE `typecompte` DISABLE KEYS */;
+INSERT INTO `typecompte` VALUES (1,'courant'),(2,'épargne');
+/*!40000 ALTER TABLE `typecompte` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-13 15:51:55
